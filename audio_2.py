f19a88be5aa5ebfe6eaafd8b2da6569576542d65
ervand7 <ervand7@mail.ru>
2020-11-27 18:32:47 +0300

    adding if __name__ == __main__:

--- audio_2.py
+++ audio_2.py
@@ -40,27 +40,28 @@ class Album:
         return sum([i.duration for i in self.list_tracks_in_album])
 
 
-album1 = Album("Первый альбом", "Первый исполнитель")
-album1.add_track_in_album(Track("Частушка", 1))
-album1.add_track_in_album(Track("Лирика", 4))
-album1.add_track_in_album(Track("Романс", 3))
+if __name__ == '__main__':
+    album1 = Album("Первый альбом", "Первый исполнитель")
+    album1.add_track_in_album(Track("Частушка", 1))
+    album1.add_track_in_album(Track("Лирика", 4))
+    album1.add_track_in_album(Track("Романс", 3))
 
-album2 = Album("Другой альбом", "Другой исполнитель")
-album2.add_track_in_album(Track("Баллада", 2))
-album2.add_track_in_album(Track("Менуэт", 5))
-album2.add_track_in_album(Track("Серенада", 5))
+    album2 = Album("Другой альбом", "Другой исполнитель")
+    album2.add_track_in_album(Track("Баллада", 2))
+    album2.add_track_in_album(Track("Менуэт", 5))
+    album2.add_track_in_album(Track("Серенада", 5))
 
-# Проверяем код:
-# Задание 1.1
-track1 = Track('Bohemian rhapsody', 6)
-print(track1)
-print()
+    # Проверяем код:
+    # Задание 1.1
+    track1 = Track('Bohemian rhapsody', 6)
+    print(track1)
+    print()
 
-# Задание 1.2
-album1.__str__()
-print()
+    # Задание 1.2
+    album1.__str__()
+    print()
 
-# Задание 2
-track1 = Track('Bohemian rhapsody', 6)
-track2 = Track('The show must go on', 4)
-print(track1 > track2)
+    # Задание 2
+    track1 = Track('Bohemian rhapsody', 6)
+    track2 = Track('The show must go on', 4)
+    print(track1 > track2)
