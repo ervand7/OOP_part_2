ceae6473235d1915b8b3b7864a9c6a72a7dd9529
ervand7 <ervand7@mail.ru>
2020-11-27 18:29:57 +0300

    Changing after a few months

--- Uncle_Joe's_Farm_2.py
+++ Uncle_Joe's_Farm_2.py
@@ -1,75 +1,1 @@
-class Animals:
-    def __init__(self, name=' ', weight=0, voice=' '):
-        self.name = name
-        self.weight = weight
-        self.voice = voice
-
-
-class Goose(Animals):
-    pass
-
-
-class Cow(Animals):
-    pass
-
-
-class Sheep(Animals):
-    pass
-
-
-class Chicken(Animals):
-    pass
-
-
-class Goat(Animals):
-    pass
-
-
-class Duck(Animals):
-    pass
-
-
-class Eggs(Goose, Chicken, Duck):
-    def collect_eggs(self):
-        for i in list_of_eggs:
-            print(
-                f'Мое призвание - это собирать яйца у существа с именем "{i.name}", весом в {i.weight} усл. ед., говорящего "{i.voice}".')
-
-
-list_of_eggs = [ # list_of_eggs - это атрибут. Проще понимается если это записать так: list_of_eggs = Eggs("Гусь Серый", 50, "Га-Га-Га") -тут 3 аргумента потому, что это задано в параметрах на стр. 2. Просто в данном случае атрибут - это список со ножеством знаений.
-    Eggs("Гусь Серый", 50, "Га-Га-Га"),
-    Eggs("Гусь Белый", 45, "Га-Га-Га"),
-    Eggs("Курица Ко-Ко", 47, "Кукареку"),
-    Eggs("Курица Кукареку", 43, "Кукареку"),
-    Eggs("Утка Кряква", 49, "Кря"),
-]
-Eggs.collect_eggs(list_of_eggs) # метод вызываем напрямую от класса и в него, соответственно помещаем нужный нам, в данном случае единственный, атрибут)
-
-
-class Milk(Cow, Goat):
-    def get_milk(self):
-        for i in list_of_milk:
-            print(
-                f'Мое призвание - это доить существо с именем "{i.name}", весом в {i.weight} усл. ед., говорящую "{i.voice}".')
-
-
-list_of_milk = [
-    Milk("Корова Манькa", 33, "Му-Му"),
-    Milk("Коза №1", 36, "Мееееееее"),
-    Milk("Коза №2", 32, "Мееееееее")
-]
-Milk.get_milk(list_of_milk)
-
-
-class Cut(Sheep):
-    def make_cut(self):
-        for i in list_of_cut:
-            print(
-                f'Мое призвание - это стричь существо с именем "{i.name}", весом в {i.weight} усл. ед., говорящее "{i.voice}".')
-
-
-list_of_cut = [
-    Cut("Овца №1", 77, "Беееее"),
-    Cut("Овца №2", 73, "Беееее")
-]
-Cut.make_cut(list_of_cut)
+class Animals:    def __init__(self, name=' ', weight=0, voice=' '):        self.name = name        self.weight = weight        self.voice = voiceclass Goose(Animals):    passclass Cow(Animals):    passclass Sheep(Animals):    passclass Chicken(Animals):    passclass Goat(Animals):    passclass Duck(Animals):    passclass Eggs(Goose, Chicken, Duck):    def collect_eggs(self):        for i in list_of_eggs:            print(                f'Мое призвание - это собирать яйца у существа с именем "{i.name}", весом в {i.weight} усл. ед., говорящего "{i.voice}".')list_of_eggs = [ # list_of_eggs - это атрибут. Проще понимается если это записать так: list_of_eggs = Eggs("Гусь Серый", 50, "Га-Га-Га") -тут 3 аргумента потому, что это задано в параметрах на стр. 2. Просто в данном случае атрибут - это список со ножеством знаений.    Eggs("Гусь Серый", 50, "Га-Га-Га"),    Eggs("Гусь Белый", 45, "Га-Га-Га"),    Eggs("Курица Ко-Ко", 47, "Кукареку"),    Eggs("Курица Кукареку", 43, "Кукареку"),    Eggs("Утка Кряква", 49, "Кря"),]Eggs.collect_eggs(list_of_eggs) # метод вызываем напрямую от класса и в него, соответственно помещаем нужный нам, в данном случае единственный, атрибут)class Milk(Cow, Goat):    def get_milk(self):        for i in list_of_milk:            print(                f'Мое призвание - это доить существо с именем "{i.name}", весом в {i.weight} усл. ед., говорящую "{i.voice}".')list_of_milk = [    Milk("Корова Манькa", 33, "Му-Му"),    Milk("Коза №1", 36, "Мееееееее"),    Milk("Коза №2", 32, "Мееееееее")]Milk.get_milk(list_of_milk)class Cut(Sheep):    def make_cut(self):        for i in list_of_cut:            print(                f'Мое призвание - это стричь существо с именем "{i.name}", весом в {i.weight} усл. ед., говорящее "{i.voice}".')list_of_cut = [    Cut("Овца №1", 77, "Беееее"),    Cut("Овца №2", 73, "Беееее")]Cut.make_cut(list_of_cut)
--- audio_2.py
+++ audio_2.py
@@ -1,4 +1,4 @@
-class Track():
+class Track:
     def __init__(self, name="Незаполненное название", duration=0):
         self.name = name
         self.duration = duration
@@ -6,8 +6,6 @@ class Track():
     def __str__(self):
         return f"{self.name} - {self.duration} min."
 
-
-# Эти 3 функции я нагуглил для сравнения длительностей. Важно их прописать в этом классе, а не в Album
     def __lt__(self, other):
         return self.duration < other.duration
 
@@ -18,60 +16,51 @@ class Track():
         return self.duration > other.duration
 
 
-
-class Album():
+class Album:
     def __init__(self, name=" ", group=" "):
         self.name = name
         self.group = group
-        self.tracks = []
+        self.list_tracks_in_album = []
 
-    def __str__(self):
-        print(f"Name group '{self.group} \nName album '{self.name}''.")
-        for s in enumerate(self.get_tracks(), 1):
-            print(f"     {s[0]}. {s[1]}")
-        print(f"Общая длительность альбома: {self.get_duration()} минут.\n")
-
-
-
-
-    def get_tracks(self):
-        return [i.__str__() for i in self.tracks]
-
-    def get_duration(self):
-        return sum([i.duration for i in self.tracks])
-
-    def add_track(self, value_from_track_class):
+    def add_track_in_album(self, value_from_track_class):
         if not isinstance(value_from_track_class, Track):
             raise NotImplementedError('ERROR OF APPENDING!!!!')
-        self.tracks.append(value_from_track_class)
+        self.list_tracks_in_album.append(value_from_track_class)
 
+    def get_tracks_for_task(self):
+        return [i.__str__() for i in self.list_tracks_in_album]
 
-albums = []
+    def __str__(self):
+        print(f"Name of group '{self.group}' \nName of album '{self.name}'")
+        for s in enumerate(self.get_tracks_for_task(), 1):
+            print(f"     {s[0]}. {s[1]}")
+        print(f"Общая длительность альбома: {self.get_duration_of_album()} минут.\n")
+
+    def get_duration_of_album(self):
+        return sum([i.duration for i in self.list_tracks_in_album])
+
 
 album1 = Album("Первый альбом", "Первый исполнитель")
-album1.add_track(Track("Частушка", 1))
-album1.add_track(Track("Лирика", 4))
-album1.add_track(Track("Романс", 3))
-albums.append(album1)
+album1.add_track_in_album(Track("Частушка", 1))
+album1.add_track_in_album(Track("Лирика", 4))
+album1.add_track_in_album(Track("Романс", 3))
 
 album2 = Album("Другой альбом", "Другой исполнитель")
-album2.add_track(Track("Баллада", 2))
-album2.add_track(Track("Менуэт", 5))
-album2.add_track(Track("Серенада", 5))
-albums.append(album2)
+album2.add_track_in_album(Track("Баллада", 2))
+album2.add_track_in_album(Track("Менуэт", 5))
+album2.add_track_in_album(Track("Серенада", 5))
 
 # Проверяем код:
 # Задание 1.1
 track1 = Track('Bohemian rhapsody', 6)
 print(track1)
 print()
+
 # Задание 1.2
 album1.__str__()
 print()
+
 # Задание 2
 track1 = Track('Bohemian rhapsody', 6)
 track2 = Track('The show must go on', 4)
 print(track1 > track2)
-
-
-
